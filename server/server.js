13c38928c5b04032aeca030d3118bac05990e53c
Sanjay Ram M <m.sanjayram07@gmail.com>
2026-03-13 12:17:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(helmet({
 
 // CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
